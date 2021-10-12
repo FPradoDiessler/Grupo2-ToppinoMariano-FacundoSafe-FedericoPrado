57c39f675eccabdfb31658909836ac36d1355417
MarianoT <marianotoppino@gmail.com>
2021-10-11 21:37:22 -0300

    confeccione la clase Medio de Pago y sub clases

---
+++ Grupo2-ToppinoMariano-FacundoSafe-FedericoPrado/MedioDePago.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grupo2_ToppinoMariano_FacundoSafe_FedericoPrado
+{
+    public abstract class MedioDePago
+    {
+
+        public abstract Boolean realizarPago();
+    }
+}
